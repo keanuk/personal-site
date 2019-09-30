bb9c5712322112437222829c42508d1b9309a577
Keanu Kerr <keanukerr@gmail.com>
2019-09-30 00:13:56 -0400

    Added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,18 @@ import { Component, Directive, HostListener, ElementRef } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent { }
+export class AppComponent { 
+
+  constructor() { }
+
+}
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 640 || document.documentElement.scrollTop > 640) {
+    document.getElementById("navbar").style.opacity = "1";
+  } else {
+    document.getElementById("navbar").style.opacity = "0";
+  }
+} 
